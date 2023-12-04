9a1b500d242d03a90340cf9b32fac6980d1e0dd7
Salazar-prime <aggarw82@purdue.edu>
2023-12-04 11:48:33 -0500

    day4

---
+++ day4-1.py
@@ -0,0 +1,19 @@
+import re
+
+data = list(open("inputs/day4-1.txt"))
+data = [x.strip().split(": ") for x in data]
+
+cardSum = 0
+
+for idx, card in enumerate(data):
+    # get card number card <card number> with regex
+    cardNo = re.search(r"\d+", card[0]).group()
+    winNo, myNo = card[1].split("|")
+    winNo, myNo = winNo.split(), myNo.split()
+
+    # get number of matches in myNo with winNo
+    matches = len(set(winNo) & set(myNo))
+    if matches > 0:
+        cardSum += 2 ** (matches - 1)
+
+print(cardSum)

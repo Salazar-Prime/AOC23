48fc1cf3447566bf7a40ba59d12685f13425d524
Salazar-prime <aggarw82@purdue.edu>
2023-12-13 08:54:29 -0500

    start day13

---
+++ day13/day13-1.py
@@ -0,0 +1,15 @@
+##### BEGIN BOIER CODE #####
+import sys, os, re
+from localUtils import *
+
+sys.path.insert(1, "common")
+from utils import *
+
+
+test = False
+currentDay = 13
+if test:
+    data = list(open(f"day{currentDay}/test.txt"))
+else:
+    data = list(open(f"day{currentDay}/day{currentDay}-1.txt"))
+##### END BOIER CODE #####

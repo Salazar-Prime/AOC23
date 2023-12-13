b501d64655a13057ccd6bb1630abd556f2feda60
Salazar-prime <aggarw82@purdue.edu>
2023-12-13 14:56:20 -0500

    still on day13

--- day13/day13-1.py
+++ day13/day13-1.py
@@ -1,5 +1,6 @@
 ##### BEGIN BOIER CODE #####
 import sys, os, re
+import numpy as np
 from localUtils import *
 
 sys.path.insert(1, "common")
@@ -13,3 +14,67 @@ if test:
 else:
     data = list(open(f"day{currentDay}/day{currentDay}-1.txt"))
 ##### END BOIER CODE #####
+ps = list(map(str.split, data.split("\n\n")))
+
+# split data into groups sepearted by \n
+groups = []
+startIdx = 0
+for idx, line in enumerate(data):
+    if line == "\n":
+        groups.append(data[startIdx:idx])
+        startIdx = idx + 1
+
+result = 0
+for gpIdx, group in enumerate(groups):
+    foundSymmetry = False
+    totalRows = len(group)
+    for idx in range(totalRows - 1):
+        if group[idx] == group[idx + 1]:
+            # found a possible symmetry line
+            foundSymmetry, startIdx, endIdx = True, idx - 1, idx + 2
+            while foundSymmetry:
+                if startIdx < 0 or endIdx >= totalRows:
+                    break
+                if group[startIdx] == group[endIdx]:
+                    startIdx -= 1
+                    endIdx += 1
+                else:
+                    foundSymmetry = False
+        if foundSymmetry:
+            result += 100 * (idx + 1)
+            print("".join(group))
+            print(f"found symmetry at {idx}")
+            print("-" * 10)
+            break
+
+# take transpose of groups
+transposedGroups = []
+for group in groups:
+    transposedGroups.append(list(map(list, zip(*group))))
+
+# delete last element of each group
+for group in transposedGroups:
+    del group[-1]
+
+for gpIdx, group in enumerate(transposedGroups):
+    foundSymmetry = False
+    totalRows = len(group)
+    for idx in range(totalRows - 1):
+        if group[idx] == group[idx + 1]:
+            # found a possible symmetry line
+            foundSymmetry, startIdx, endIdx = True, idx - 1, idx + 2
+            while foundSymmetry:
+                if startIdx < 0 or endIdx >= totalRows:
+                    break
+                if group[startIdx] == group[endIdx]:
+                    startIdx -= 1
+                    endIdx += 1
+                else:
+                    foundSymmetry = False
+        if foundSymmetry:
+            result += idx + 1
+            # print("".join(group))
+            print(f"found symmetry at {idx}")
+            print("-" * 10)
+            break
+print(result)

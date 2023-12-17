ae82d756ab692326ad19a0e0505d91140b384d10
Salazar-prime <aggarw82@purdue.edu>
2023-12-16 19:45:17 -0500

    add

--- common/utils.py
+++ common/utils.py
@@ -1,9 +1,53 @@
 import re
+import numpy as np
+
+
+def addT(a, b):
+    # check if a and b are tuples and equal length
+    if not isinstance(a, tuple) or not isinstance(b, tuple):
+        raise TypeError("a and b must be tuples")
+    if len(a) != len(b):
+        raise ValueError("a and b must be of equal length")
+    return tuple(sum(x) for x in zip(a, b))
 
 
 def getNumbersFromString(str):
     # returns a list of nubers from a string
     return [int(s) for s in re.findall(r"-?\d+", str)]
+
+
+def convertToNumpyArrayStr(data, removeNewLine=True):
+    # get length of each row and total number of rows
+    totalRows = len(data)
+    totalCols = len(data[0])
+    if removeNewLine:
+        totalCols -= 1
+    # create numpy array
+    arr = np.zeros((totalRows, totalCols), dtype=str)
+    # populate numpy array
+    for idx, row in enumerate(data):
+        for jdx, char in enumerate(row):
+            if removeNewLine and char == "\n":
+                continue
+            arr[idx][jdx] = char
+    return arr
+
+
+def convertToNumpyArrayInt(data, removeNewLine=True):
+    # get length of each row and total number of rows
+    totalRows = len(data)
+    totalCols = len(data[0])
+    if removeNewLine:
+        totalCols -= 1
+    # create numpy array
+    arr = np.zeros((totalRows, totalCols), dtype=int)
+    # populate numpy array
+    for idx, row in enumerate(data):
+        for jdx, char in enumerate(row):
+            if removeNewLine and char == "\n":
+                continue
+            arr[idx][jdx] = char
+    return arr
 
 
 def saveToFile(data, saveName, newLine=True):

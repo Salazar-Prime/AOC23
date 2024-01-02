4a699151cc66adc0194a83ab93b6f823a2b39e0d
Salazar-prime <aggarw82@purdue.edu>
2024-01-02 13:58:41 -0500

    add all days

---
+++ day22/day22-1.py
@@ -0,0 +1,23 @@
+##### BEGIN BOIER CODE #####
+# fmt: off
+import sys, os, re
+from localUtils import *
+
+sys.path.insert(1, "common")
+from utils import *
+
+
+test = False
+currentDay = 22
+if test:
+    data = list(open(f"day{currentDay}/test.txt"))
+else:
+    data = list(open(f"day{currentDay}/day{currentDay}-1.txt"))
+
+# Convert to numpy array Str/Int
+# data = convertToNumpyArray(data, dtype=str)
+# create a grid dicitonary od data with coordinates as keys and data as values
+# grid, gridW, gridH = gridify(data, int)
+
+# fmt:on
+##### END BOIER CODE #####
